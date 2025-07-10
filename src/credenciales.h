6729b315d2ba18f83ad39439941fc1df7b2eed47
Jose Hernandez <jhernandez59@gmail.com>
2025-07-10 16:38:59 -0500

    Ignorar archivos sensibles correctamente

--- src/credenciales.h
+++
@@ -1,5 +0,0 @@
-// ------------------------------
-// Archivo: credenciales.h
-// ------------------------------
-#define WIFI_SSID "SKYNET"
-#define WIFI_PASSWORD "79152302"
--- src/secrets.h
+++
@@ -1,7 +0,0 @@
-#ifndef SECRETS_H
-#define SECRETS_H
-
-#define FIREBASE_HOST "esp8266-sensor-8d08f-default-rtdb.firebaseio.com"
-#define FIREBASE_KEY "AIzaSyCtXrGlTEXBzQdbYTFSE0vOEJKmYj1AAf4"
-
-#endif
